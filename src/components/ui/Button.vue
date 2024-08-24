b1b9aafb350dbf15e4ff011f562a05faaadb414f
prog <mks.shi@ya.ru>
2024-08-24 23:41:32 +0300

    ui button and textfield

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-btn
+        :disabled="disabled"
+        :loading="loading"
+        :color="color ?? 'success'"
+        size="large"
+        type="submit"
+        block
+    >
+        <slot></slot>
+    </v-btn>
+</template>
+
+<script setup>
+const props = defineProps({
+    disabled: Boolean,
+    loading: Boolean,
+    color: String
+})
+</script>
---
+++ src/components/ui/TextField.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-text-field 
+        clearable
+        :label="label"
+        :rules="rules"
+        variant="outlined"
+        :placeholder="placeholder"
+        @click:clear="emit('clear')"
+        @input="updateValue"
+        :type="type"
+        :prepend-inner-icon="icon"
+    />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    modelValue: String,
+    label: String,
+    placeholder: String,
+    type: String,
+    icon: String,
+    rules: Array
+});
+
+const emit = defineEmits(['update:modelValue', 'clear']);
+
+const updateValue = (e) => emit('update:modelValue', e.target.value)
+</script>
+
+<style lang="scss">
+</style>

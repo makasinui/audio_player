a16585df12980e5c6827ad5c834a51866dd40c9f
prog <mks.shi@ya.ru>
2024-08-24 23:41:45 +0300

    add layout for auth

---
+++ src/components/auth/AuthLayout.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-app>
+        <v-theme-provider theme="dark" class="pa-10">
+            <Header :left-navigation="false" :page="props.page" />
+            <v-main>
+                <v-container fluid class="container auth-page">
+                    
+                    <v-card
+                        elevation="8"
+                        min-width="448"
+                        rounded="lg"
+                        class="form"
+                    >
+                        <slot />
+                    </v-card>
+                </v-container>
+            </v-main>
+        </v-theme-provider>
+    </v-app>
+</template>
+
+<script setup>
+import Header from "./Header.vue";
+const props = defineProps({
+    page: String,
+});
+</script>
+
+<style lang="scss">
+.v-main {
+    background: #070827;
+
+    .auth-page {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: white;
+
+        .form {
+            padding: 15px;
+            padding-top: 35px;
+        }
+    }
+}
+</style>
---
+++ src/components/auth/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="header-auth">
+        <v-app-bar color="#0b0b31" class="container header-auth__container">
+            <div class="title">
+                <v-toolbar-title>{{ page }}</v-toolbar-title>
+            </div>
+        </v-app-bar>
+    </header>
+</template>
+
+<script setup>
+defineProps({
+    page: String,
+});
+</script>
+
+<style lang="scss">
+.header-auth {
+    .v-toolbar__content {
+        padding: 0 20px;
+    }
+
+    .title {
+        width: 100%;
+        text-align: center;
+    }
+}
+</style>
